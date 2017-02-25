93382980b535c855a08dc82888e5a1df4d0f7b00
sirfeedsalot <rchhay@mail.sfsu.edu>
2017-02-24 20:45:45 -0800

    made some changes

---
+++ cmake-build-debug/CMakeFiles/01_pointers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_pointers.dir/main.cpp.o"
+  "01_pointers.pdb"
+  "01_pointers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_pointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -12,20 +12,21 @@ namespace example1 {
     void example() {
         //pointers basic 1
         //TODO 1 - create var1 of type int
+        int var1;
         //TODO 1 - create a character array var2 that can hold 10 elements
-
-
-        //TODO 1
-        cout << "Size of var1 variable: ";
+        char var2 [10];
 
         //TODO 1
-        cout << "Size of var2 variable: ";
+        // cout << "Size of var1 variable: "sizeof(var1) << endl;
 
         //TODO 1
-        cout << "Address of var1 variable: ";
+        //cout << "Size of var2 variable: "sizeof(var2) << endl;
 
         //TODO 1
-        cout << "Address of var2 variable: ";
+        cout << "Address of var1 variable: " << &var1 << endl;
+
+        //TODO 1
+        cout << "Address of var2 variable: " << &var2<< endl;
 
     }
 }
@@ -34,25 +35,25 @@ namespace example2 {
     void example() {
         //pointers basic 2
         //TODO 2 - create a integer variable var holding some value
+        int var = 0;
         //TODO 2 - create a pointer of type int
-
+        int* ip;
         //TODO 2 - store address of var into pointer
-
+        ip = &var;
         //TODO 2
-        cout << "Value of var variable: ";
+        cout << "Value of var variable: " << var<<endl;
 
         //TODO 2 // print the address stored in ip pointer variable
-        cout << "Address stored in ip variable: ";
+        cout << "Address stored in ip variable: " << ip<<endl;
 
         //TODO 2// access the value at the address available in pointer
-        cout << "Value of *ip variable: ";
+        cout << "Value of *ip variable: " << *ip<<endl;
 
         //TODO 2
-        cout << "Address of *ip variable: ";
+        cout << "Address of *ip variable: " << &ip<<endl;
 
         //TODO 2 - set the pointer to null pointer
-
-        cout << "The value of ptr is ";
+        //cout << "The value of ptr is " << nullptr;
     }
 }
 
@@ -61,19 +62,21 @@ namespace example3 {
         //pointer addition
 
         //TODO 3 - integer array of size MAX
+        int arr[MAX];
         //TODO 3 - integer pointer
-
+        int* ptr = arr;
         // let us have array address in pointer.
 
         //TODO 3 - point pointer to variable
         for (int i = 0; i < MAX; i++) {
             //TODO 3
-            cout << "Address of var[" << i << "] = ";
+            cout << "Address of var[" << i << "] = " << ptr << endl;
 
             //TODO 3
-            cout << "Value of var[" << i << "] = ";
+            cout << "Value of var[" << i << "] = " << *ptr << endl;
 
             //TODO 3// point to the next location
+            cout << ++ptr << endl;
         }
     }
 }
@@ -84,19 +87,21 @@ namespace example4 {
         //pointer subtraction
 
         //TODO 4 - integer array of size MAX
+        int arr [MAX];
         //TODO 4 - integer pointer
-
+        int* ptr;
         //TODO 4// let us have address of the last element in pointer.
-
+        ptr = &arr[MAX];
         for (int i = MAX; i > 0; i--) {
             //TODO 4
-            cout << "Address of var[" << i << "] = ";
+            cout << "Address of var[" << i << "] = " << ptr << endl;
 
             //TODO 4
-            cout << "Value of var[" << i << "] = ";
+            cout << "Value of var[" << i << "] = " << *ptr << endl;
 
             //TODO 4
             // point to the previous location
+            cout << --ptr << endl;
         }
     }
 }
@@ -107,22 +112,23 @@ namespace example5 {
         //comparing pointers
 
         //TODO 5 - integer array of size MAX
+        int array[MAX];
         //TODO 5 - integer pointer
-
+        int* ptr;
         //TODO 5// let us have address of the first element in pointer.
         int i = 0;
+        ptr = &array[i];
         //TODO 5 iterate from &variable[MAX-1] to ptr
-        while (42) {
+        for(int j = MAX+1; j >= i; j--) {
 
             //TODO 5
-            cout << "Address of var[" << i << "] = ";
+            cout << "Address of var[" << i << "] = " << ptr << endl;
 
             //TODO 5
-            cout << "Value of var[" << i << "] = ";
+            cout << "Value of var[" << i << "] = " << *ptr << endl;
 
             //TODO 5// point to the next location
-
-            i++;
+            ++ptr;
         }
     }
 }
@@ -160,13 +166,16 @@ namespace example8 {
         //array of pointers
         int var[MAX] = {10, 100, 200};
         //TODO 8 define an integer pointer of size MAX
+        int* ptr[MAX];
 
         for (int i = 0; i < MAX; i++) {
             //TODO 8// assign the address of integer.
+            ptr[i] = &var[i];
         }
         for (int i = 0; i < MAX; i++) {
             //TODO 8
             cout << "Value of var[" << i << "] = ";
+            cout << *ptr[i] << endl;
         }
     }
 }
@@ -177,6 +186,7 @@ namespace example9 {
         //array of names
 
         //TODO 9 - create a character pointer array of size MAX
+        char* ptr[MAX];
 
         for (int i = 0; i < MAX; i++) {
             //TODO 9
@@ -191,7 +201,7 @@ namespace example10 {
         int var;
         int *ptr;
         //TODO 10 - create a pointer to pointer
-
+        int **pptr = &ptr;
 
         var = 3000;
 
@@ -199,12 +209,13 @@ namespace example10 {
         ptr = &var;
 
         //TODO 10// take the address of ptr using address of operator &
+        cout<< "Adress of ptr: "<< &ptr << endl;
 
         // take the value using pptr
         cout << "Value of var :" << var << endl;
         cout << "Value available at *ptr :" << *ptr << endl;
         //TODO 10
-        cout << "Value available at **pptr :";
+        cout << "Value available at **pptr :" << **pptr << endl;
 
     }
 }
@@ -301,20 +312,19 @@ namespace example13 {
 
 int main() {
 
-    //example1::example();
-    //example2::example();
-    //example3::example();
-    //example4::example();
-    //example5::example();
-    //example6::example();
-    //example7::example();
-    //example8::example();
-    //example9::example();
-    //example10::example();
-    //example11::example();
-    //example12::example();
-    //example13::example();
-
+//    example1::example();
+//    example2::example();
+//    example3::example();
+//    example4::example();
+//    example5::example();
+//    example6::example();
+//    example7::example();
+//    example8::example();
+//    example9::example();
+//    example10::example();
+//    example11::example();
+//    example12::example();
+//    example13::example();
 
     return 0;
 }
